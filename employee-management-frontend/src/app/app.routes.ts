6100dea4fe5bd2fff0b6f843fd3eda8aae942c06
MesfinGithinji <meshgithinji@gmail.com>
2024-02-27 01:04:50 +0300

    Configure routes

--- employee-management-frontend/src/app/app.routes.ts
+++ employee-management-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/:id', component: EmployeeDetailsComponent },
+  { path: 'create', component: EmployeeCreateComponent },
+  { path: 'edit/:id', component: EmployeeEditComponent }
+];
