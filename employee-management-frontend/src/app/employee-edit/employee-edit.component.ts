bfbfca829322db5142100de7889e5d956e7507cd
MesfinGithinji <meshgithinji@gmail.com>
2024-02-27 15:28:13 +0300

    Component for editing employee details , update functionality

--- employee-management-frontend/src/app/employee-edit/employee-edit.component.ts
+++ employee-management-frontend/src/app/employee-edit/employee-edit.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-employee-edit',
-  standalone: true,
-  imports: [],
   templateUrl: './employee-edit.component.html',
-  styleUrl: './employee-edit.component.css'
+  styleUrls: ['./employee-edit.component.css']
 })
-export class EmployeeEditComponent {
+export class EmployeeEditComponent implements OnInit {
+  employee: Employee | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: EmployeeService
+  ) { }
+
+  ngOnInit(): void {
+    this.getEmployee();
+  }
+
+  getEmployee(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.employeeService.getEmployee(id)
+      .subscribe(employee => this.employee = employee);
+  }
+
+  updateEmployee(name: string, email: string, jobTitle: string, phone: string, imageUrl: string, employeeCode: string): void {
+    if (!this.employee) {
+      return;
+    }
+    this.employee.name = name;
+    this.employee.email = email;
+    this.employee.jobTitle = jobTitle;
+    this.employee.phone = phone;
+    this.employee.imageUrl = imageUrl;
+    this.employee.employeeCode = employeeCode;
+    this.employeeService.updateEmployee(this.employee)
+    .subscribe(() => this.router.navigate(['/employees']));
+  }
 }
