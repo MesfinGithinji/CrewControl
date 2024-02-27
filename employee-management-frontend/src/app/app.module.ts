4693d63d7d41169a779d654a1130658ef24bec46
MesfinGithinji <meshgithinji@gmail.com>
2024-02-27 15:23:41 +0300

    Configiure the module config , both declarations and imports

--- employee-management-frontend/src/app/app.module.ts
+++ employee-management-frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'; // Import RouterModule
 import { appRoutes } from './app.routes'; // Import routes
-import { HttpClientModule } from '@angular/common/http'; // Import HTTP client
+import { HttpClientModule } from '@angular/common/http';// Import HTTP client
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
