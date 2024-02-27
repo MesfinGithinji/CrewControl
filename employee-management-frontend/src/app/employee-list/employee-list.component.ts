4bb21ecba5d141773900be3763c74065410c6dd5
MesfinGithinji <meshgithinji@gmail.com>
2024-02-27 15:28:52 +0300

    Component that allows us to list employees in the system

--- employee-management-frontend/src/app/employee-list/employee-list.component.ts
+++ employee-management-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee.model';
 
 @Component({
   selector: 'app-employee-list',
-  standalone: true,
-  imports: [],
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
 
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  getEmployees(): void {
+    this.employeeService.getEmployees()
+      .subscribe(employees => this.employees = employees);
+  }
 }
