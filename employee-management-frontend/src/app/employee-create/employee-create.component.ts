f5a36b95d6a91a26ab0d719092885d230695464c
MesfinGithinji <meshgithinji@gmail.com>
2024-02-28 19:26:16 +0300

    employee-create component

--- employee-management-frontend/src/app/employee-create/employee-create.component.ts
+++ employee-management-frontend/src/app/employee-create/employee-create.component.ts
@@ -9,14 +9,23 @@ import { Employee } from '../employee.model';
   styleUrls: ['./employee-create.component.css']
 })
 export class EmployeeCreateComponent {
+  employee: Employee = {
+    id: 0,
+    name: '',
+    email: '',
+    jobTitle: '',
+    phone: '',
+    imageUrl: '',
+    employeeCode: ''
+  };
+
   constructor(
     private router: Router,
     private employeeService: EmployeeService
   ) { }
 
-  createEmployee(name: string, email: string, jobTitle: string, phone: string, imageUrl: string, employeeCode: string): void {
-    const newEmployee: Employee = { name, email, jobTitle, phone, imageUrl, employeeCode } as Employee;
-    this.employeeService.createEmployee(newEmployee)
+  createEmployee(): void {
+    this.employeeService.createEmployee(this.employee)
       .subscribe(() => this.router.navigate(['/employees']));
   }
 }
