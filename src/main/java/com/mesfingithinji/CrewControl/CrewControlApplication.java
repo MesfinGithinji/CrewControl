d2c3f200b1200dea00f9578fff21234a6f231ea8
MesfinGithinji <meshgithinji@gmail.com>
2024-03-01 13:13:41 +0300

    OPENAPI Standard Documentation

--- src/main/java/com/mesfingithinji/CrewControl/CrewControlApplication.java
+++ src/main/java/com/mesfingithinji/CrewControl/CrewControlApplication.java
@@ -1,5 +1,9 @@
 package com.mesfingithinji.CrewControl;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +11,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.List;
-
-import static java.util.Arrays.*;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "CrewControl Employee Management System",
+				version = "1.0.0",
+				description = "This API provides endpoints for managing employees within the Crew Control application. It allows users to perform CRUD (Create, Read, Update, Delete) operations on employee records, including retrieving all employees, finding employees by ID, adding new employees, updating existing employee details, and deleting employees.",
+				termsOfService = "By accessing or using the Crew Control API (\"API\"), you agree to comply with these Terms of Service (\"Terms\"). If you do not agree to these Terms, you may not use the API.",
+				contact = @Contact(
+						name = "Mesfin Githinji",
+						email = "mesfingithinji@gmail.com"
+				)
+		)
+)
 public class CrewControlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrewControlApplication.class, args);
@@ -21,13 +36,13 @@ public class CrewControlApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(asList("Origin", "Content-Type", "Accept", "Authorization",
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
 				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
