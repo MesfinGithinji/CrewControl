06af5ff2eeb736020f045edc01f82d71e8326ced
MesfinGithinji <meshgithinji@gmail.com>
2024-03-04 15:45:43 +0300

    Components service addition

--- employee-management-frontend/src/app/app.module.ts
+++ employee-management-frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router'; // Import RouterModule
 import { appRoutes } from './app.routes'; // Import routes
 import { HttpClientModule } from '@angular/common/http';// Import HTTP client
-import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeCreateComponent } from './employee-create/employee-create.component';
 import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
     EmployeeListComponent,
     EmployeeDetailsComponent,
     EmployeeCreateComponent,
@@ -25,6 +25,6 @@ import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
     FormsModule // Import forms functionality
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
